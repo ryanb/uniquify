a56df922cd348b2a95abe75069349447196a5fe5
Rodrigo Navarro <rnavarro1@gmail.com>
2010-12-22 15:16:12 -0200

    Creating token before the record validation

--- lib/uniquify.rb
+++ lib/uniquify.rb
@@ -2,20 +2,20 @@ module Uniquify
   def self.included(base)
     base.extend ClassMethods
   end
-  
+
   def ensure_unique(name)
     begin
       self[name] = yield
     end while self.class.exists?(name => self[name])
   end
-  
+
   module ClassMethods
-    
+
     def uniquify(*args, &block)
       options = { :length => 8, :chars => ('a'..'z').to_a + ('A'..'Z').to_a + ('0'..'9').to_a }
       options.merge!(args.pop) if args.last.kind_of? Hash
       args.each do |name|
-        before_create do |record|
+        before_validation :on => :create do |record|
           if block
             record.ensure_unique(name, &block)
           else
@@ -26,7 +26,7 @@ module Uniquify
         end
       end
     end
-    
+
   end
 end
 
